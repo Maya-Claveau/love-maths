c8fe021b3673069d052f06168a252915778700bd
Maya-Claveau <maya.claveau@gmail.com>
2021-09-20 08:52:05 +0000

    Set up structure of game functions

---
+++ assets/js/scripts.js
@@ -0,0 +1,32 @@
+function funGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMiltiplyQuestion() {
+
+}
+
