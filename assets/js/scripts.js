f4ea731a07e2262cda0857347d8b9bd20664352d
Maya-Claveau <maya.claveau@gmail.com>
2021-10-05 12:46:50 +0000

    Add code to check the answer and respond to the user

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer ();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -25,8 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 function runGame(gameType) {
 
     // Creates two random numbers between 1 and 24
-    let num1 = Math.floor(Math.random() * 25) +1;
-    let num2 = Math.floor(Math.random() * 25) +1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -36,8 +36,23 @@ function runGame(gameType) {
     }
 } 
 
+/**
+* Checks the answer against the first element in
+* the returned calculateCorrectAnswer array 
+*/
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
